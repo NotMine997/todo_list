a0ad8e50767aaf0df6cedb9294840ff0babb83ad
NotMine997 <vova2201012@gmail.com>
2026-08-20 18:00:07 +0300

    Updated

---
+++ app/layouts/main.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+    <div class="flex min-h-screen flex-col bg-white text-neutral-900 dark:bg-neutral-950 dark:text-neutral-50">
+    <HeaderMain />
+        <slot />
+    <Footer />
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+import HeaderMain from '~/components/HeaderMain.vue';
+import Footer from '~/components/Footer.vue';
+
+
+
+</script>
+
+<style>
+
+</style>
